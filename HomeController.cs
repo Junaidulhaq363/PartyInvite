f06dfcded7bfabbe2e62c72c10399ee194f28ec4
Junaidulhaq363 <66385932+Junaidulhaq363@users.noreply.github.com>
2021-11-15 10:43:07 +0530

    Add files via upload

--- HomeController.cs
+++ HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
+using System.Linq;
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
@@ -12,14 +13,23 @@ namespace PartyInvites.Controllers
         public ViewResult RsvpForm()
         {
             return View();
-
         }
-
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            // TODO: store response from guest
-            return View();
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
