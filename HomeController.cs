1bd61034dca62eee76585e0cb51a61252cc1ec14
Junaidulhaq363 <66385932+Junaidulhaq363@users.noreply.github.com>
2021-11-15 10:06:27 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: store response from guest
+            return View();
+        }
+    }
+}
